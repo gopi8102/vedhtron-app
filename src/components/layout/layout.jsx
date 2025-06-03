e602cc1d098aa967b96226df53f97fecf984c672
gopi8102 <kishangopi810@gmail.com>
2025-06-03 12:34:12 +0530

    nochange

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -5,7 +5,7 @@ import Footer from '../footer/footer'
 function Layout({children}) {
   return (
     
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8" >
+    <div  >
         <Navbar />
         <div className="content">
           {children}
